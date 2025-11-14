fdaf80ac80a73d309cdba34aecd13e0e5524ed19
Ilya <56804640+anrej0705@users.noreply.github.com>
2025-11-14 03:00:03 +0300

    USART переведён с программного на железный уровень

--- main/main.c
+++ main/main.c
@@ -2,6 +2,7 @@
 #include "main.h"
 #include "BA63.h"												//Основной экран отображения
 #include "TIM1.h"												//Таймер, останавливающий двигатель по достижении нужного количества импульсов
+#include "TIM3.h"												//Таймер опроса различных АНАЛоговых датчиков
 #include "TIM4.h"												//Управление светодиодами и экраном
 #include "TIM6.h"												//Миллисекундная задержка
 #include "TIM7.h"												//Опрос кнопок
@@ -13,15 +14,22 @@
 
 #include "presets.h"
 #include "locale_ru.h"
+#include "global_vars.h"
 
 void welcome(void);
 
 int main(void)
 {
+	ba63_fifo.remain = 223;
+	ba63_fifo.size = 224;
+	ba63_fifo.tx_en = 0;
+	ba63_fifo.stop = 0;
+	
 	//Начинаем настройку железок
 	setup_gpio();
 	setup_usart();
 	
+	setup_sensor_poll();
 	setup_led_screen_update();
 	
 	setup_delay_ms();
@@ -32,13 +40,17 @@ int main(void)
 	setup_feed_coil();
 	setup_take_coil();
 	
+	NVIC_SetPriority(TIM1_UP_TIM16_IRQn, 0);
+	NVIC_SetPriority(TIM2_IRQn, 1);
+	NVIC_SetPriority(TIM3_IRQn, 1);
+	NVIC_SetPriority(TIM4_IRQn, 1);
+	NVIC_SetPriority(TIM7_IRQn, 1);
+	
 	//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 	//Настраиваем таймеры
 	
 	//Врубаем тактирование
 	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);
 	
 	//Настраиваем таймеры
 	TIM_TimeBaseInitTypeDef m_tim;
@@ -53,16 +65,6 @@ int main(void)
 	m_tim.TIM_Prescaler = RGBY_PSC;
 	m_tim.TIM_Period = RGBY_PWM_RANGE;
 	TIM_TimeBaseInit(TIM2, &m_tim);
-	
-	//TIM3 - опрос датчиков натяжения
-	m_tim.TIM_Prescaler = TIM3_PSC;
-	m_tim.TIM_Period = TIM3_ARR;
-	TIM_TimeBaseInit(TIM3, &m_tim);
-	
-	//TIM4 - опрос датчика камеры
-	m_tim.TIM_Prescaler = TIM4_PSC;
-	m_tim.TIM_Period = TIM4_ARR;
-	TIM_TimeBaseInit(TIM4, &m_tim);
 	
 	//Настраивае ШИМ где надо
 	TIM_OCInitTypeDef m_pwm;
@@ -82,34 +84,82 @@ int main(void)
 	//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 	//Настраиваем АЦП
 	
-	//Начинаем продрачивание
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2ENR_ADC1EN, ENABLE);
+	RCC_AHBPeriphClockCmd(RCC_AHBENR_DMA1EN, ENABLE);
+	
+	ADC_InitTypeDef m_adc;
+	DMA_InitTypeDef m_dma;
+	
+	ADC_StructInit(&m_adc);
+	DMA_StructInit(&m_dma);
+	
+	m_dma.DMA_BufferSize = 4;
+	m_dma.DMA_DIR = DMA_DIR_PeripheralSRC;
+	m_dma.DMA_M2M = DMA_M2M_Disable;
+	m_dma.DMA_MemoryBaseAddr = (uint32_t)&adc_buffer;
+	m_dma.DMA_MemoryDataSize = DMA_MemoryDataSize_HalfWord;
+	m_dma.DMA_MemoryInc = DMA_MemoryInc_Enable;
+	m_dma.DMA_Mode = DMA_Mode_Circular;
+	m_dma.DMA_PeripheralBaseAddr = (uint32_t)&ADC1->DR;
+	m_dma.DMA_PeripheralDataSize = DMA_PeripheralDataSize_HalfWord;
+	m_dma.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
+	m_dma.DMA_Priority = DMA_Priority_High;
+	DMA_Init(DMA1_Channel1, &m_dma);
+	DMA_Cmd(DMA1_Channel1, ENABLE);
+	
+	m_adc.ADC_ContinuousConvMode = ENABLE;
+	m_adc.ADC_DataAlign = ADC_DataAlign_Right;
+	m_adc.ADC_ExternalTrigConv = ADC_ExternalTrigConv_None;
+	m_adc.ADC_Mode = ADC_Mode_Independent;
+	m_adc.ADC_NbrOfChannel = 4;
+	m_adc.ADC_ScanConvMode = ENABLE;
+	ADC_Init(ADC1, &m_adc);
+	
+	ADC_RegularChannelConfig(ADC1, ADC_Channel_4, 1, ADC_SampleTime_239Cycles5);
+	ADC_RegularChannelConfig(ADC1, ADC_Channel_5, 2, ADC_SampleTime_239Cycles5);
+	ADC_RegularChannelConfig(ADC1, ADC_Channel_TempSensor, 3, ADC_SampleTime_239Cycles5);
+	ADC_RegularChannelConfig(ADC1, ADC_Channel_Vrefint, 4, ADC_SampleTime_239Cycles5);
+	ADC_Cmd(ADC1, ENABLE);
+	ADC_DMACmd(ADC1, ENABLE);
+	
+	ADC_TempSensorVrefintCmd(ENABLE);
+	
+	
+	//Калибруемся, в перерывах между калибровками пинаем хуи
+	ADC_ResetCalibration(ADC1);
+	while(ADC_GetResetCalibrationStatus(ADC1))
+	{
+		//NOP
+	}
+	ADC_StartCalibration(ADC1);
+	while(ADC_GetCalibrationStatus(ADC1))
+	{
+		//NOP
+	}
+	
+	ADC_Cmd(ADC1, ENABLE);
+	ADC_DMACmd(ADC1, ENABLE);
+	ADC_SoftwareStartConvCmd(ADC1, ENABLE);
 	//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 	
 	//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 	//Запускаемся
-	//TIM_Cmd(TIM1, ENABLE);
 	TIM_Cmd(TIM2, ENABLE);
-	TIM_Cmd(TIM3, ENABLE);
-	TIM_Cmd(TIM4, ENABLE);
-	TIM_Cmd(TIM5, ENABLE);
 	USART_Cmd(USART1, ENABLE);
-	//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+	//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------	
 		
 	//Запуски
 	start_key_poller();
 	
+	start_sensor_poll();
 	start_led_screen_update();
 	
-	start_ttm();
 	start_ttm_controller();
-	start_feed_coil();
-	start_take_coil();
 	
-	set_speed_ttm(200);
+	set_speed_ttm(800);
 	set_speed_feed_coil(800);
 	set_speed_take_coil(800);
-		
+	
 	//Ждём пока экран раскочегарится, нагреется крч приведёт себя в готовность
 	delay_ms(366);																															//Ждём пока всё загрузится и просрётся
 	BA63_Init();																																//Чистим экран от мусора
@@ -126,53 +176,53 @@ void welcome(void)
 {
 	//Начинаем разворачивание заголовка
 	BA63_SetPos(0,0);
-	BA63_SendString(device_name_frame1);
+	BA63_SendString(device_name_frame1, sizeof(device_name_frame1));
 	delay_ms(50);
 	BA63_SetPos(0,0);
-	BA63_SendString(device_name_frame2);
+	BA63_SendString(device_name_frame2, sizeof(device_name_frame2));
 	delay_ms(50);
 	BA63_SetPos(0,0);
-	BA63_SendString(device_name_frame3);
+	BA63_SendString(device_name_frame3, sizeof(device_name_frame3));
 	delay_ms(50);
 	BA63_SetPos(0,0);
-	BA63_SendString(device_name_frame4);
+	BA63_SendString(device_name_frame4, sizeof(device_name_frame4));
 	delay_ms(50);
 	BA63_SetPos(0,0);
-	BA63_SendString(device_name_frame5);
+	BA63_SendString(device_name_frame5, sizeof(device_name_frame5));
 	delay_ms(50);
 	BA63_SetPos(0,0);
-	BA63_SendString(device_name_frame6);
+	BA63_SendString(device_name_frame6, sizeof(device_name_frame6));
 	
 	//В процессе разработки мне регулярно снилась одна милфа(SR) так что её имя будет кодовым для проекта
 	BA63_SetPos(0,1);
-	BA63_SendString(projectCode);
+	BA63_SendString(projectCode, sizeof(projectCode));
 	
 	delay_ms(950);
 	
 	//Сворачиваем заголовок
 	BA63_SetPos(0,0);
-	BA63_SendString(device_name_frame5);
+	BA63_SendString(device_name_frame5, sizeof(device_name_frame5));
 	delay_ms(50);
 	BA63_SetPos(0,0);
-	BA63_SendString(device_name_frame4);
+	BA63_SendString(device_name_frame4, sizeof(device_name_frame4));
 	delay_ms(50);
 	BA63_SetPos(0,0);
-	BA63_SendString(device_name_frame3);
+	BA63_SendString(device_name_frame3, sizeof(device_name_frame3));
 	delay_ms(50);
 	BA63_SetPos(0,0);
-	BA63_SendString(device_name_frame2);
+	BA63_SendString(device_name_frame2, sizeof(device_name_frame2));
 	delay_ms(50);
 	BA63_SetPos(0,0);
-	BA63_SendString(device_name_frame1);
+	BA63_SendString(device_name_frame1, sizeof(device_name_frame1));
 	
 	delay_ms(300);
 	BA63_SetPos(0,0);
 	BA63_DeleteToEndline();
 	BA63_SetPos(0,1);
-	BA63_SendString(fwVer);
+	BA63_SendString(fwVer, sizeof(fwVer));
 	delay_ms(1250);
 	BA63_SetPos(0,1);
-	BA63_SendString(author);
+	BA63_SendString(author, sizeof(author));
 	delay_ms(1250);
 	
 	//Приветствие завершено
