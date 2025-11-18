8dbbc734a5024bd2ef4d2c4270cda1481305e163
Ilya <56804640+anrej0705@users.noreply.github.com>
2025-11-18 03:06:51 +0300

    Дебажные фишки

--- periph/TIM4.c
+++ periph/TIM4.c
@@ -121,21 +121,21 @@ void update_screen(void)
 	cached_adc_vref = adc_vref;
 	cached_tim1_pulses_cnt = tim1_pulses_cnt;
 	
-	sprintf(sensor_val, "%04d", take_coil_current_speed);
-	strncpy(&screen_buf.first[0], "X0:", 3);
-	strncpy(&screen_buf.first[3], sensor_val, 4);
+	sprintf(sensor_val, "%04d", debug_take_coil_arr);
+	strncpy(&screen_buf.first[0], "ARR:", 4);
+	strncpy(&screen_buf.first[4], sensor_val, 4);
 	
-	sprintf(sensor_val, "%04d", take_coil_tension_sensor);
-	strncpy(&screen_buf.first[10], "X1:", 2);
-	strncpy(&screen_buf.first[13], sensor_val, 4);
+	sprintf(sensor_val, "%04d", debug_take_coil_ccr1);
+	strncpy(&screen_buf.first[10], "CCR1:", 5);
+	strncpy(&screen_buf.first[15], sensor_val, 4);
 	
-	sprintf(sensor_val, "%04d", feed_coil_current_speed);
-	strncpy(&screen_buf.second[0], "X0:", 3);
-	strncpy(&screen_buf.second[3], sensor_val, 4);
+	sprintf(sensor_val, "%04d", debug_feed_coil_arr);
+	strncpy(&screen_buf.second[0], "ARR:", 4);
+	strncpy(&screen_buf.second[4], sensor_val, 4);
 	
-	sprintf(sensor_val, "%04d", feed_coil_tension_sensor);
-	strncpy(&screen_buf.second[10], "X1:", 2);
-	strncpy(&screen_buf.second[13], sensor_val, 4);
+	sprintf(sensor_val, "%04d", debug_feed_coil_ccr1);
+	strncpy(&screen_buf.second[10], "CCR1:", 5);
+	strncpy(&screen_buf.second[15], sensor_val, 4);
 	
 	//Обновляем экран
 	BA63_SetPos(0, 0);
