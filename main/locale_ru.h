cf719db57598731770e8c68c25554b537880d692
Ilya <56804640+anrej0705@users.noreply.github.com>
2025-11-23 05:21:17 +0300

    Добавлены новые сообщения

--- main/locale_ru.h
+++ main/locale_ru.h
@@ -3,11 +3,45 @@
 
 #include "stdint.h"
 
-uint8_t device_name_frame1[21] = "      \313\363\367-1-35      ";
-uint8_t device_name_frame2[21] = "    \222 \313\363\367-1-35 \221    ";
-uint8_t device_name_frame3[21] = "   \220\222 \313\363\367-1-35 \221\220   ";
-uint8_t device_name_frame4[21] = "  \220\220\222 \313\363\367-1-35 \221\220\220  ";
-uint8_t device_name_frame5[21] = " \220\220\220\222 \313\363\367-1-35 \221\220\220\220 ";
-uint8_t device_name_frame6[21] = "\220\220\220\220\222 \313\363\367-1-35 \221\220\220\220\220";
+extern const char string_empty[21];
+
+extern const char device_name_frame1[21];
+extern const char device_name_frame2[21];
+extern const char device_name_frame3[21];
+extern const char device_name_frame4[21];
+extern const char device_name_frame5[21];
+extern const char device_name_frame6[21];
+extern const char device_name_frame7[21];
+
+extern const char ru_debug_adc[5];
+
+extern const char ru_color_preset_template[21];
+extern const char ru_frames_template[21];
+extern const char ru_color_preset_hint[21];
+extern const char ru_ready[21];
+extern const char ru_paused[21];
+extern const char ru_finish[21];
+extern const char ru_remote_ctrl[21];
+
+extern const char ru_service_menu_1[21];
+extern const char ru_service_menu_2[21];
+extern const char ru_service_menu_3[21];
+extern const char ru_service_menu_4[21];
+extern const char ru_service_menu_5[21];
+extern const char ru_service_menu_6[21];
+extern const char ru_service_menu_7[21];
+extern const char ru_service_menu_8[21];
+extern const char ru_service_menu_9[21];
+extern const char ru_service_menu_10[21];
+extern const char ru_service_menu_11[21];
+extern const char ru_service_menu_12[21];
+
+extern const char ru_alarm_ttm_engine_overheat[21];
+extern const char ru_alarm_feed_engine_overheat[21];
+extern const char ru_alarm_take_engine_overheat[21];
+extern const char ru_alarm_ttm_break[21];
+extern const char ru_alarm_feed_break[21];
+extern const char ru_alarm_take_break[21];
+extern const char ru_alarm_general_feilor[21];
 
 #endif
