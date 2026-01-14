5889c4758a27a9848162c25cf60f93cfb7e55cbe
Ilya <56804640+anrej0705@users.noreply.github.com>
2026-01-14 21:50:57 +0300

    Обновлена версия прошивки и год выпуска

--- main/main.h
+++ main/main.h
@@ -4,9 +4,9 @@
 #include "stdint.h"
 
 char projectCode[21] = "RAIDEN MEI[V3]  35mm";
-char fwVer[21] = "FW: REL_1.0  12.2025";
+char fwVer[21] = "FW: REL_1.1  01.2026";
 char author[21] = "by poheron & VCD    ";
-char year[21] = "Discord:FTC |`24 `25";
+char year[21] = "Discord:FTC |`24 `26";
 char del[21] = "                     ";
 
 #endif
